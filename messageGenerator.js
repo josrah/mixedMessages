94d4fbb6f38fa0b3ac818861f9fc29a2894da160
josrah <josefin@rahm.nu>
2021-06-29 00:40:31 +0200

    Added words in arrays

--- messageGenerator.js
+++ messageGenerator.js
@@ -7,8 +7,11 @@ know of JavaScript syntax so far to build the program
 and customize it to your liking. */
 
 //Where is my phone? Message consists of preposition, room, and furniture
-prepositions = [];
-rooms = [];
-furniture = [];
+prepositions = ["above", "across", "along", "around", "at", "behind", "below", "beneath", "beside", "beyond", "by", "off", "on", "opposite", "over", "past", "under"];
+rooms = ["attic", "basement", "bathroom", "bedroom", "boiler room", "cloakroom", "common room", "dining room", "family room", "garage", "guest room", "gym", "hallway", "kitchen", "laundry room", "library", "living room", "lounge", "nursery", "office", "pantry", "wine cellar"];
+furniture = ["amrchair", "bassinet", "bean bag chair", "bed", "bench", "bookshelf", "bunk bed", "cabinet", "carpet", "chair", "couch", "cupboard", "curtains", "desk", "dresser", "end table", "futon", "hammock", "hutch", "kitchen island", "lamp", "lounge chair", "mirror", "mattress", "office chair", "ottoman", "pillow", "sofa", "sideboard", "table", "vanity", "wardrobe", "waterbed"];
 
 //Function that randomizes each of the words, and puts them together to a message
+function generateMessage () {
+
+}
