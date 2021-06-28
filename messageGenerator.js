4981d23b098189fb0a967896ddaa3ec35dc9506e
josrah <josefin@rahm.nu>
2021-06-29 00:15:05 +0200

    first commit

---
+++ messageGenerator.js
@@ -0,0 +1,14 @@
+/* For this project, you will build a message generator 
+program. Every time a user runs a program, they should 
+get a new, randomized output. To make your program truly 
+random, the message that it outputs should be made up of 
+at least three different pieces of data. Take what you 
+know of JavaScript syntax so far to build the program 
+and customize it to your liking. */
+
+//Where is my phone? Message consists of preposition, room, and furniture
+prepositions = [];
+rooms = [];
+furniture = [];
+
+//Function that randomizes each of the words, and puts them together to a message
